463d30378cf18bc4557f16fdfcc82498d5b568c1
Kundzia13 <aleksandra.bobowska.93@gmail.com>
2019-07-07 22:17:08 +0200

    first commit

---
+++ src/main/java/pl/akademiakodu/reporting/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package pl.akademiakodu.reporting.repository;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.akademiakodu.reporting.model.Role;
+
+
+@Primary
+@Repository("roleRepository")
+public interface RoleRepository extends JpaRepository<Role,Integer> {
+    Role findByRole(String role);
+}
+
---
+++ src/main/java/pl/akademiakodu/reporting/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package pl.akademiakodu.reporting.repository;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.akademiakodu.reporting.model.User;
+
+@Primary
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User,Integer > {
+    User findByEmail(String email);
+}
